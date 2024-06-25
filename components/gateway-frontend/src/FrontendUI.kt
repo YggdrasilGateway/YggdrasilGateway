9120303e66d70cafd21517cb481bbee8c7f09e4b
Karlatemp <kar@kasukusakura.com>
2024-06-25 19:38:12 +0800

    Improve develop frontend forward

--- components/gateway-frontend/src/FrontendUI.kt
+++ components/gateway-frontend/src/FrontendUI.kt
@@ -8,15 +8,32 @@ import io.ktor.client.engine.java.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.utils.io.*
 import java.net.URI
 
 @EventSubscriber
 internal object FrontendUI {
     private val httpClient = HttpClient(Java)
+
+    private fun Headers.dropProxiedHeaders(): Headers {
+        val thiz = this
+        return Headers.build {
+            thiz.forEach { key, value ->
+                if (HttpHeaders.ContentType.equals(key, ignoreCase = true)) {
+                    return@forEach
+                }
+                if (HttpHeaders.ContentLength.equals(key, ignoreCase = true)) {
+                    return@forEach
+                }
+                appendAll(key, value)
+            }
+        }
+    }
 
     @EventSubscriber.Handler(priority = EventPriority.LOWEST)
     fun YggdrasilHttpServerInitializeEvent.ModuleInitializeEvent.handle() {
@@ -31,25 +48,32 @@ internal object FrontendUI {
             app.routing {
                 head("{...}") {
                     val resp = httpClient.head(Url(URI.create(FrontendProperties.devUrl).resolve(call.request.uri)))
-                    call.response.status(resp.status)
-                    resp.headers.forEach { s, strings ->
-                        strings.forEach { v ->
-                            call.response.headers.append(s, v)
-                        }
-                    }
-                    call.respond(resp.bodyAsChannel())
+                    val contentType = resp.contentType()
+                    val contentLength = resp.contentLength()
+
+                    call.respond(object : OutgoingContent.NoContent() {
+                        override val contentLength: Long? = contentLength
+                        override val contentType: ContentType? get() = contentType
+                        override val headers: Headers get() = resp.headers.dropProxiedHeaders()
+                        override val status: HttpStatusCode get() = resp.status
+                    })
                 }
 
                 get("{...}") {
                     val resp = httpClient.get(Url(URI.create(FrontendProperties.devUrl).resolve(call.request.uri)))
-                    call.response.status(resp.status)
-                    resp.headers.forEach { s, strings ->
-                        strings.forEach { v ->
-                            call.response.headers.append(s, v)
-                        }
-                    }
+                    val contentType = resp.contentType()
+                    val contentLength = resp.contentLength()
 
-                    call.respond(resp.bodyAsChannel())
+                    call.respond(object : OutgoingContent.WriteChannelContent() {
+                        override val contentLength: Long? = contentLength
+                        override val contentType: ContentType? get() = contentType
+                        override val headers: Headers get() = resp.headers.dropProxiedHeaders()
+                        override val status: HttpStatusCode get() = resp.status
+
+                        override suspend fun writeTo(channel: ByteWriteChannel) {
+                            resp.bodyAsChannel().copyAndClose(channel)
+                        }
+                    })
                 }
             }
 
